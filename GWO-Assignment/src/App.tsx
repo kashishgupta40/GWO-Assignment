92ba9af97ab2349b2bfd2330ec0f3f1b6f4e8105
Kashish Gupta <guptakashishiit40@gmail.com>
2024-07-10 01:56:08 +0530

    save

---
+++ GWO-Assignment/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DepartmentList from './DepartmentalList.tsx';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <DepartmentList />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+export default App;
--- GWO-Assignment/src/main.tsx
+++ GWO-Assignment/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import App from './App';
+import App from './App';
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 
 const theme = createTheme();
@@ -9,7 +9,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      
+      <App />
     </ThemeProvider>
   </React.StrictMode>
 );
